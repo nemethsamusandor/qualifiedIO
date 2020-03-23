6376c71d822d0d0eff2b7961abeba409ec90ba78
Sándor Németh <extern.sandor.nemeth@porscheinformatik.at>
2020-03-23 13:03:14 +0100

    Rename project

--- DiskSpace/src/DiskSpace.java
+++ DiskSpace/src/DiskSpace.java
@@ -5,7 +5,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class EmptySpace
+public class DiskSpace
 {
     public static void main(String[] args)
     {
